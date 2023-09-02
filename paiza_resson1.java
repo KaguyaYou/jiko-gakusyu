9265e475667f4f02a9951cf108000023e6f816d2
KaguyaYou <caose906@gmail.com>
2023-09-02 22:41:48 +0900

    paizaで勉強

---
+++ paiza_resson1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        int n = Integer.parseInt(line);
+        for (int i = 0; i < n; i++) {
+            String ans = sc.nextLine();
+            System.out.println(ans);
+        }
+        
+        
+    }
+}
